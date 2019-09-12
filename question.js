a1f37cf0fd69732179cb4e32381a86c7cd1755e8
kyrs <kumarshubham652@gmail.com>
2019-09-12 20:18:19 +0530

    new change on use of have by agnius

--- question.js
+++ question.js
@@ -7,12 +7,12 @@ var randomQues = [
 "<p> Thank you for your answer.</p> Now, can you please tell me why we should hire you.",
 "<p> Thank you for your answer.</p> Now, can you please tell me what your greatest strengths are.",
 "<p> Thank you for your answer.</p> Now, can you please tell me what you consider to be your weaknesses.",
-"<p> Thank you for your answer.</p> Now, can you please tell me about a challenge or conflict you've faced at work or during your studies, and how you dealt with it.",
+"<p> Thank you for your answer.</p> Now, can you please tell me about a challenge or conflict you have faced at work or during your studies, and how you dealt with it.",
 "<p> Thank you for your answer.</p> Now, can you please tell me how you deal with pressure or stressful situations.",
 "<p> Thank you for your answer.</p> Now, can you please tell me what your ideal job would be.",
 "<p> Thank you for your answer.</p> Now, can you please tell me how you handle criticism.",
 "<p> Thank you for your answer.</p> Now, can you please tell me what your best boss and your worst boss would be.",
-"<p> Thank you for your answer.</p> Now, can you please tell me more about your work style. Do you like working in a team environment or do you prefer working alone ?",
+"<p> Thank you for your answer.</p> Now, can you please tell me more about your work style. Do you like working in a team environment or do you prefer working alone?",
 "<p> Thank you for your answer.</p> Now, can you please tell me what challenges you are looking for in a job.",
 "<p> Thank you for your answer.</p> Now, can you please tell me what you have learned from your mistakes.",
 "<p> Thank you for your answer.</p> Now, can you please tell me about a difficult work situation or project you experienced and how you overcame it.",
@@ -20,9 +20,9 @@ var randomQues = [
 "<p> Thank you for your answer.</p> Now, can you please tell me about a time you disagreed with a decision and what you did.",
 "<p> Thank you for your answer.</p> Now, can you please tell me what you like to do outside of work.",
 "<p> Thank you for your answer.</p> Now, can you please tell me how people usually describe you.",
-"<p> Thank you for your answer.</p> Now, can you please tell me the most important responsibilities you’ve had to handle.",
+"<p> Thank you for your answer.</p> Now, can you please tell me the most important responsibilities you have had to handle.",
 "<p> Thank you for your answer.</p> Now, can you please tell me what goals, including career goals, you have set for yourself.",
-"<p> Thank you for your answer.</p> Now, can you please tell me about a time when you had too much to do, but not enough resources (such as time or money). How did you handle the pressure and achieve goals ?",
-"<p> Thank you for your answer.</p> Now, can you please tell me about a time you had to deal with a team member who constantly opposed your ideas. How did you handle it ?"
+"<p> Thank you for your answer.</p> Now, can you please tell me about a time when you had too much to do, but not enough resources (such as time or money). How did you handle the pressure and achieve goals?",
+"<p> Thank you for your answer.</p> Now, can you please tell me about a time you had to deal with a team member who constantly opposed your ideas. How did you handle it?"
 ];
 var randomQuesToAsk = 2;
